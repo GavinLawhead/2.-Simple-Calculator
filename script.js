6905534547d2b4c1166516ff91f5f2bdb850b8be
GavinLawhead <Lawhead.gavin@live.jobcorps.org>
2022-07-01 18:31:45 +0000

    may need to change the script, may need to add a script tag in html, use testrepo as template

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const prevOperand = document.querySelector("previous-operand");
-const currantOperand = document.querySelector("current-operand");
+// const currantOperand = document.querySelector("current-operand");
 const number = document.querySelectorAll("data-number");
 const operations = document.querySelectorAll("data-operation");
-const delete = document.querySelector('data-delete');
+// const delete = document.querySelector('data-delete');
 const clear = document.querySelector("data-all-clear");
 const equals = document.querySelector("data-equals");
+
+const element = document.querySelectorAll("current-operand");
+element.addEventListener("click", textnumbers());
+
+function textnumbers() {
+  document.querySelectorAll("data-number").innerHTML = number;
+}
