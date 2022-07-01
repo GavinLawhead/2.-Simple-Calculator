1628e4018d65720e3ffe70e33c0d4b66e5204a18
GavinLawhead <Lawhead.gavin@live.jobcorps.org>
2022-07-01 13:59:22 +0000

    working o the function

---
+++ script.js
@@ -0,0 +1,7 @@
+const prevOperand = document.querySelector("previous-operand");
+const currantOperand = document.querySelector("current-operand");
+const number = document.querySelectorAll("data-number");
+const operations = document.querySelectorAll("data-operation");
+const delete = document.querySelector('data-delete');
+const clear = document.querySelector("data-all-clear");
+const equals = document.querySelector("data-equals");
