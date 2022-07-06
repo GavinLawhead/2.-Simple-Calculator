e51ed9cddd4ce93890878a2db8c217c50c41e760
55746 <Lawhead.gavin@live.jobcorps.org>
2022-07-06 14:04:50 +0000

    i tested the buttons, and the function, and it works, just need to make it appear in the right place

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const prevOperand = document.querySelector("previous-operand");
-// const currantOperand = document.querySelector("current-operand");
-const number = document.querySelectorAll("data-number");
-const operations = document.querySelectorAll("data-operation");
-// const delete = document.querySelector('data-delete');
-const clear = document.querySelector("data-all-clear");
-const equals = document.querySelector("data-equals");
+// const prevOperand = document.querySelector("previous-operand");
+// // const currantOperand = document.querySelector("current-operand");
+// const number = document.querySelectorAll("data-number");
+// const operations = document.querySelectorAll("data-operation");
+// // const delete = document.querySelector('data-delete');
+// const clear = document.querySelector("data-all-clear");
+// const equals = document.querySelector("data-equals");
 
-const element = document.querySelectorAll("current-operand");
-element.addEventListener("click", textnumbers());
+// const element = document.querySelectorAll("current-operand");
+// element.addEventListener("click", textnumbers());
 
-function textnumbers() {
-  document.querySelectorAll("data-number").innerHTML = number;
-}
+// function textnumbers() {
+//   document.querySelectorAll("data-number").innerHTML = number;
+// }
